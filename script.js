edaded9bb090ff4c865b12d0ba04c382f1c0a5cb
Cayla Bradley <caylabradleydev@gmail.com>
2024-03-13 12:57:35 -0500

    Modal Functionality

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnHideModal = document.querySelector(".hide-modal");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+//Hide Modal
+function hideModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+//Open Modal
+function showModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", showModal);
+}
+
+//Modal Buttons
+overlay.addEventListener("click", hideModal);
+btnHideModal.addEventListener("click", hideModal);
+
+//Hide Modal w/ Escape Key
+document.addEventListener("keydown", function (e) {
+  if ((e.key === "Escape") & !modal.classList.contains("hidden")) {
+    hideModal();
+  }
+});
